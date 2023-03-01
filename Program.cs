86f7888800bd72625ec7dc07dea91ec3cae857bb
Lcuky12 <118757873+Lcuky12@users.noreply.github.com>
2023-03-01 16:14:15 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfMessage;
+            string userInput;
+
+            Console.WriteLine("Введите сообщение");
+            userInput = Console.ReadLine();
+            Console.WriteLine("Введите количество повторов");
+            numberOfMessage = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < numberOfMessage; i++)
+            {
+                Console.WriteLine(userInput);
+            }
+        }
+    }
+}
